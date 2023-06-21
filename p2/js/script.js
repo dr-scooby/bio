43236902b083643bc44b8780b326f3bc7b7a3f10
dr-scooby <hinurali@hotmail.com>
2023-06-21 11:15:35 -0700

    second portfolio web page

---
+++ p2/js/script.js
@@ -0,0 +1,37 @@
+
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+}
+
+// scroll sections
+let sections = document.querySelectorAll('section'); // get all the sections
+
+// the nav links are in the header -> nav -> a
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            // active navbar links
+            navLinks.forEach(links =>{
+                 links.classList.remove('active');
+                 document.querySelector('header nav a[href*=' + id + '] ').classList.add('active');
+            });
+        }
+    });
+
+
+    // sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
